8b166feeee76d15f4f6ba6b4994edbf7fba8428e
Ger-Martinez <ger.ariel.martinez@gmail.com>
2018-12-09 14:12:38 -0300

    Boton para cambiar de nivel

--- src/game/frontend/CandyFrame.java
+++ src/game/frontend/CandyFrame.java
@@ -92,6 +92,8 @@ public class CandyFrame extends VBox {
 						} else {
 							message = message + " Finished - Loser !";
 						}
+						NextLevelWindow next = new NextLevelWindow();
+						next.addButtons();
 						// Aca podriamos meter alerta que sea tipo:
 						// ganaste! queres ir al next level o queres salir?
 						// perdiste! reintentar? si o no
