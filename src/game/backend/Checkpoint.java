ebe75774c93dbfc71edb634f7f4ce6efd10c5076
AgustinRoca <agusroca99@hotmail.com>
2018-12-10 02:21:34 -0300

    Calculo automatico de las values en Checkpoint

--- src/game/backend/Checkpoint.java
+++ src/game/backend/Checkpoint.java
@@ -1,24 +1,23 @@
 package game.backend;
 
 public enum Checkpoint {
-	//pensar si no conviene cambiar el value con un 2^ordinal()
-	U(-1,0, 1), // arriba
-	UU(-2,0, 2), // arriba x2
-	D(1,0, 4), // abajo
-	DD(2,0, 8), // abajo x2
-	R(0,1, 16), // der
-	RR(0,2, 32), // der x2
-	L(0,-1, 64), // izq
-	LL(0,-2, 128); // izq x2
+	U(-1,0), // arriba
+	UU(-2,0), // arriba x2
+	D(1,0), // abajo
+	DD(2,0), // abajo x2
+	R(0,1), // der
+	RR(0,2), // der x2
+	L(0,-1), // izq
+	LL(0,-2); // izq x2
 	
 	private final int i;
 	private final int j;
 	private final int value;
 	
-	Checkpoint(int i, int j, int value) {
+	Checkpoint(int i, int j) {
 		this.i = i;
 		this.j = j;
-		this.value = value;
+		this.value = (int)Math.pow(2, ordinal());
 	}
 	
 	public int getI() {
